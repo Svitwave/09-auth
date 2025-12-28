996ced4b897ea6c0c1dc93f4e16f3063eabf55d8
Svitwave <svitlanamaz@gmail.com>
2025-12-28 16:58:29 +0200

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,72 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { parse } from "cookie";
+import { checkServerSession } from "./lib/api/serverApi";
+
+const privateRoutes = ["/profile", "/notes", "/notes/filter"];
+const publicRoutes = ["/sign-in", "/sign-up"];
+
+export async function middleware(request: NextRequest) {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+
+  // Шлях, на який користувач намагається перейти
+  const { pathname } = request.nextUrl;
+  const isPrivateRoute = privateRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+  const isPublicRoute = publicRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  if (isPrivateRoute) {
+    if (!accessToken) {
+      if (refreshToken) {
+        // Отримуємо нові cookie
+        const data = await checkServerSession();
+        const setCookie = data.headers["set-cookie"];
+
+        if (setCookie) {
+          const cookieArray = Array.isArray(setCookie)
+            ? setCookie
+            : [setCookie];
+          for (const cookieStr of cookieArray) {
+            const parsed = parse(cookieStr);
+            const options = {
+              expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
+              path: parsed.Path,
+              maxAge: Number(parsed["Max-Age"]),
+            };
+            if (parsed.accessToken)
+              cookieStore.set("accessToken", parsed.accessToken, options);
+            if (parsed.refreshToken)
+              cookieStore.set("refreshToken", parsed.refreshToken, options);
+          }
+
+          // важливо — передаємо нові cookie далі, щоб оновити їх у браузері
+          return NextResponse.next({
+            headers: {
+              Cookie: cookieStore.toString(),
+            },
+          });
+        }
+      }
+
+      // немає жодного токена — редірект на сторінку входу
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+  }
+
+  if (isPublicRoute && accessToken) {
+    // Якщо користувач аутентифікований і намагається потрапити на публічний маршрут, редірект на /profile
+    return NextResponse.redirect(new URL("/profile", request.url));
+  }
+
+  // Все інші випадки — дозволяємо доступ
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/profile/:path*", "/notes/:path*", "/sign-in", "/sign-up"],
+};
