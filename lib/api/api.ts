3060f182b2bd238a4e2f6c9366f34a255c74af6f
Svitwave <svitlanamaz@gmail.com>
2025-12-28 16:47:58 +0200

    fix url

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Створюємо інстанс axios
 export const nextServer = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   // baseURL: 'http://localhost:3000/api',
   withCredentials: true, // дозволяє axios працювати з cookie
 });
